a38189ad946912b09e86a73b47f613d9bc081474
meenohosn <112025098+meenohosn@users.noreply.github.com>
2022-08-24 16:58:06 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,21 @@
+/*************************************************
+*Hello.java
+*************************************************
+*Meeno Hosn
+*8-24-22
+*CMSC-255-001
+*************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+	System.out.println("Meeno Hosn");
+	System.out.println("macOS x64");
+	System.out.println("Hello.java");
+	System.out.println("8-24-22");
+	System.out.println("CMSC-255-001");
+	System.out.println();
+
+	}
+
+}
